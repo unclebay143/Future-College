27bba7f6ba986288756910aa15843d7b3284fe72
Samuel <unclebay143@gmail.com>
2020-09-22 22:00:35 +0100

    object saving to local storage, working on edit function

--- assets/js/for-loop.js
+++ assets/js/for-loop.js
@@ -27,54 +27,120 @@ let studentDb = // Student Database
 ]
 
 
-let studentRecord = () =>
+let viewRecord = () =>
 { // list of all students in the studentDb (array)
-  var tempListContainer = "";
+  var tempRecord = "";
   for(i=0; i < studentDb.length; i++)
   {
-    tempListContainer +=
+    tempRecord +=
     `
       <div class="profile" id="${i}">
-        <div class"student_details ${studentDb[i].name}profile">
-          <img src='${studentDb[i].img}' alt=${studentDb[i].name}>
+        <div class="student_details ${studentDb[i].name}profile">
+          <img src='${studentDb[i].img}' alt="${studentDb[i].name}">
         </div>
-        <div class"student_details ${studentDb[i].name}profile">
-          <strong>Admission Status</strong>:<span>${studentDb[i].admissionStatus}</span>
+        <div class="student_details ${studentDb[i].name}profile">
+          <strong>Admission Status</strong>: <span>${studentDb[i].admissionStatus}</span>
         </div>
-        <div class"student_details ${studentDb[i].name}profile">
-          <strong>Name</span>:<span>${studentDb[i].name}</span>
+        <div class="student_details ${studentDb[i].name}profile">
+          <strong>Name</strong>: <span>${studentDb[i].name}</span>
         </div>
-        <div class"student_details ${studentDb[i].name}profile">
-          <strong>Department</strong>:<span>${studentDb[i].department}</span>
+        <div class="student_details ${studentDb[i].name}profile">
+          <strong>Department</strong>: <span>${studentDb[i].department}</span>
         </div>
-        <div class"student_details ${studentDb[i].name}profile">
-          <strong>Grade Point</strong>:<span>${studentDb[i].gradePoint}</span
-        div>
+        <div class="student_details ${studentDb[i].name}profile">
+          <strong>Grade Point</strong>: <span>${studentDb[i].gradePoint}</span>
+        </div>
         <div class="btn">
-          <button onClick="removeStudent(${i})">Rusticate</button>
-          <button onClick="removeStudent(${i})">Edit Record</button>
-        <div>
+          <button class="del-btn" onClick="removeStudent(${i})">Rusticate</button>
+          <button class="edit-btn" onClick="updateStudentRecord(${i})">Edit Record</button>
+        </div>
       </div>
     `  
   };
-  // studentDb.push(tempListContainer);
-  document.getElementById("students").innerHTML = tempListContainer;
+  document.getElementById("students").innerHTML = tempRecord;
 };
 
-studentRecord(); // revoke function listOfStudent
+viewRecord(); // revoke function listOfStudent
 
-// Function to delete student record
+/* 
 
-let removeStudent = (studentId) =>
-{
-  studentDb.splice(studentId, 1);
-  studentRecord()
-}
+  >>>> Add new student after admission <<<<
 
-// Add new student after admission
+*/
 
 let newStudentEntry = () =>
 {
+  // binding values from form to variable for validation 
+  name = document.getElementById("fullname").value;
+  department = document.getElementById("department").value;
+  gradePoint = document.getElementById("gradepoint").value;
 
+ 
+  if (!name && !department && !gradePoint){  // empty form validation control 
+    alert("can't be empty")
+  }
+  else 
+  {
+    let newRecord =
+    { // binding values to array object keys
+      "name" : name,
+      "department" : department,  
+      "gradePoint" : gradePoint,
+      "img" : "assets/img/4.jpg",
+      "admissionStatus" : "Admitted"
+    }
+    studentDb.push(newRecord);
+    viewRecord();
 }
 
+  }
+  
+/* 
+  >>>> Function to delete student record <<<<
+*/
+let removeStudent = (studentId) =>
+{
+  dangerZone = confirm(`
+  Are you sure you want to delete ${studentDb[studentId].name}'s record?
+  Warning: this action is irreversible.`)
+  if (dangerZone == true)
+  {
+    studentDb.splice(studentId, 1); // locate and delete the pass id
+    viewRecord() // show updated student database
+  }
+  else
+  {
+    return;
+  }
+  
+}
+
+/* 
+  Function to Edit student entry
+*/
+
+let updateStudentRecord = (studentId) =>
+{
+  let updateRecord = 
+    {
+      "name" : prompt("Update name:", studentDb[studentId].name),
+      "department" : prompt("Change Department (takes effect after 2 working days):", studentDb[studentId].department),
+      "gradePoint" : alert(`Change Grade Point ${studentDb[studentId].gradePoint} : "Only HOD can perform this action`),
+      "img" : "assets/img/4.jpg",
+      "admissionStatus" : prompt(`Current Admission Status: ${studentDb[studentId].admissionStatus}
+        Type Status
+          1. Admitted
+          2. Graduate
+          3. Expel (Temporary)
+          4. Expel (Permanent)
+      `)
+    }
+    confirm(`
+    Confirm Changes
+    1.  ${studentDb[studentId].name} to ${updateRecord.name}
+    2.  ${studentDb[studentId].department} to ${updateRecord.department}
+    3.  ${studentDb[studentId].admissionStatus} to ${updateRecord.admissionStatus}
+    `)
+    studentDb[studentId] = updateRecord;
+    viewRecord();
+}
