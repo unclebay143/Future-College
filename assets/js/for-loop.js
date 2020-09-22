49c89d4880aa8d2a70c86e1d3dfd72d88df440e9
Samuel <unclebay143@gmail.com>
2020-09-22 13:24:07 +0100

    No issue with deleting

--- assets/js/for-loop.js
+++ assets/js/for-loop.js
@@ -1,118 +1,80 @@
+// PROGRAM THAT PERFORM CRUD ON VERSATILE MEMORY
 
-
-// Array of teams
-
-let team = [
+let studentDb = // Student Database
+[ 
   {
-    "fullName" : "Ayodele S. Adebayo",
-    "stack" : "Frontend",
-    "location" : "Abuja",
-    "img" : "/assets/img/1.jpg",
-    "phone" : "090827162355",
+    "name" : "Ayodele Samuel Adebayo",
+    "department" : "Computer Science",
+    "gradePoint" : "3.6",
+    "img" : "assets/img/3.jpeg",
+    "admissionStatus" : "Admitted"
   },
   {
-    "fullName" : "Durojaye Toba",
-    "stack" : "Backend",
-    "location" : "Kwara",
-    "img" : "/assets/img/3.jpeg",
-    "phone" : "090827162355",
+    "name" : "Durojaye Felix Toba",
+    "department" : "Computer Science",
+    "gradePoint" : "3.7",
+    "img" : "assets/img/5.jpg",
+    "admissionStatus" : "Admitted"
+
   },
   {
-    "fullName" : "Adedayo Steven Adeolu",
-    "stack" : "UI Design",
-    "location" : "Kwara",
-    "img" : "/assets/img/4.jpg",
+    "name" : "Ayodele Samuel Adebayo",
+    "department" : "Political Science",
+    "gradePoint" : "3.8",
+    "img" : "assets/img/4.jpg",
+    "admissionStatus" : "Admitted"
   }
 ]
 
-newMate = {// add new team mate from code
-  "fullName" : "Aremu Aishat",
-    "stack" : "Product Manager",
-    "location" : "Lagos",
-    "img" : "/assets/img/2.jpg",
-}
-team.push(user)
 
-teamDetails = ''
-
-
-/*
-    FOR LOOP
-      - Faster
-*/
-// time speed
-console.time("for_loop");
-    
-for(i=0; i<team.length; i++){
-   teamDetails += `<div class="profile-container" id="${i}">
-  <img src="${team[i].img}" alt="${team[i].fullName} Image"> <br>
-  <div ="txt-info">
-    <strong>Full Name</strong> : <span class="info">${team[i].fullName}</span> <br>
-    <strong>Stack</strong> : <span class="info">${team[i].stack}</span> <br>
-    <strong>Location</strong> : <span class="info">${team[i].location}</span> <br>
-    <div class="btn-container">
-      <button class="btn del-btn" onclick="deleteMember(${i});">Delete</button>
-      <button class="btn edit-btn">Edit</button>
-    </div>
-  </div>
-  </div><hr>` 
-}
-document.getElementById("theTeam").innerHTML = teamDetails;
-
-// time speed
-console.timeEnd("for_loop");
-
-
-
-/*EXERCISE*/
-
-// Function to add new user from input
-
- let addNew = () => {
-      // team = ''
-      let newName = document.getElementById("name").value;
-      let newStack = document.getElementById("stack").value;
-      let newLocation = document.getElementById("location").value;
-      let newTel = document.getElementById("tel").value;
-
-      newUser = {
-        "fullName" : newName,
-        "stack" : newStack,
-        "location" : newLocation,
-        "phone" : newTel,
-      }
-
-      team.push(newUser);
-        teamDetails += `<div class="profile-container" id="${i}">
-          <img src="/assets/img/3.jpeg" alt="${newUser.fullName} Image"> <br>
-          <div ="txt-info">
-            <strong>Full Name</strong> : <span class="info">${newUser.fullName}</span> <br>
-            <strong>Stack</strong> : <span class="info">${newUser.stack}</span> <br>
-            <strong>Location</strong> : <span class="info">${newUser.location}</span> <br>
-            <div class="btn-container">
-              <button class="btn del-btn" onclick="deleteMember(${i});">Delete</button>
-              <button class="btn edit-btn">Edit</button>
-            </div>
-          </div>
-          </div><hr>` 
-        document.getElementById("theTeam").innerHTML = teamDetails;    
-        document.getElementById("addbtn").addEventListener("click", addNew) 
-
-    };
-
-// ****** DELETE FUNCTION *******
-
-function deleteMember(x){
-sure = confirm("Are you sure you want to delete this?")
-if (sure == true){
-  del = document.getElementById(x);
-  team.splice(x,1)
-  del.remove()
-}else{
-  return;
-}
-
+let studentRecord = () =>
+{ // list of all students in the studentDb (array)
+  var tempListContainer = "";
+  for(i=0; i < studentDb.length; i++)
+  {
+    tempListContainer +=
+    `
+      <div class="profile" id="${i}">
+        <div class"student_details ${studentDb[i].name}profile">
+          <img src='${studentDb[i].img}' alt=${studentDb[i].name}>
+        </div>
+        <div class"student_details ${studentDb[i].name}profile">
+          <strong>Admission Status</strong>:<span>${studentDb[i].admissionStatus}</span>
+        </div>
+        <div class"student_details ${studentDb[i].name}profile">
+          <strong>Name</span>:<span>${studentDb[i].name}</span>
+        </div>
+        <div class"student_details ${studentDb[i].name}profile">
+          <strong>Department</strong>:<span>${studentDb[i].department}</span>
+        </div>
+        <div class"student_details ${studentDb[i].name}profile">
+          <strong>Grade Point</strong>:<span>${studentDb[i].gradePoint}</span
+        div>
+        <div class="btn">
+          <button onClick="removeStudent(${i})">Rusticate</button>
+          <button onClick="removeStudent(${i})">Edit Record</button>
+        <div>
+      </div>
+    `  
+  };
+  // studentDb.push(tempListContainer);
+  document.getElementById("students").innerHTML = tempListContainer;
 };
 
+studentRecord(); // revoke function listOfStudent
 
+// Function to delete student record
+
+let removeStudent = (studentId) =>
+{
+  studentDb.splice(studentId, 1);
+  studentRecord()
+}
+
+// Add new student after admission
+
+let newStudentEntry = () =>
+{
+
+}
 
